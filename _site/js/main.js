9e3735baed9bec0e8723663bf5f9ab1c158050d4
Wanyakio <Wanyakio@users.noreply.github.com>
2017-07-09 09:48:26 +0800

    netlify

---
+++ _site/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  $('h1').hide().delay(500).fadeIn(1400);
+
+console.log('Document Ready!');
+  //click on the hamburger menu
+  $('.fa-bars').click(function(){
+    // log
+    console.log('Hamburger clicked!');
+
+  //change visibility of navigation
+  $('#mobile-nav').css({
+    visibility:'visible'
+  });
+
+  });
+  //click the 'x'
+  //close the mobile navigation
+  $("#mobile-nav *").click(function(){
+    $('#mobile-nav').css({
+      visibility:'hidden'
+    });
+  })
+});
